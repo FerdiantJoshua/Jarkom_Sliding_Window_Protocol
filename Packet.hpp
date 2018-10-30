7697907916d6ac35d974fd6cdbc3def149a8fe72
Ferdiant Joshua <ferdiant@localhost.localdomain>
2018-10-30 17:38:29 +0700

    reformat packet.h to packet.hpp, change struct to class and add methods

---
+++ Packet.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+#define SOH (0x1)
+
+class Packet {
+	private:
+		uint8_t soh = (0x1);
+		uint32_t seqNum = 0;
+		uint32_t dataLength = 0;
+		uint8_t data = 0;
+		uint8_t checksum = 0;
+		uint8_t cat = 0;
+
+	public:
+		Packet();
+		Packet(uint32_t seqNum, uint32_t dataLength, uint8_t data);
+		Packet(const Packet& _packet);
+		~Packet();
+		Packet& operator=(const Packet& _packet);
+		void Print() const;
+		bool validate() const;
+};
