48c473f305b8329322daeb02061437546396b65f
Ferdiant Joshua <ferdiant@localhost.localdomain>
2018-10-30 17:40:06 +0700

    finish basic socket communication

---
+++ recvfile.cpp
@@ -0,0 +1,53 @@
+#include "Constants.hpp"
+
+#include "Ack.hpp"
+#include "Packet.hpp"
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+#define PORT 21234
+
+using namespace std;
+
+int main (int argc, char const *argv[]) {
+    struct sockaddr_in address;
+    struct sockaddr_in sender_address;
+    socklen_t addrlen = sizeof(sender_address);
+    int recvlen;
+    int fd = 0;
+
+    Packet *buffer = (Packet*) malloc(BUFFER_SIZE * sizeof(Packet));
+
+    //Create UDP socket
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        cout << "Socket creation error" << endl;
+        return -1;
+    }
+
+    //Bind socket to any IP and a port
+    memset((char *) &address, '0', sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY); //INADDR_ANY = 0.0.0.0 = any available IP address
+    address.sin_port = htons(PORT);
+
+    if (bind(fd, (struct sockaddr *) &address, sizeof(address)) < 0 ) {
+        cout << "Bind error" << endl;
+        return -1;
+    }
+
+    //Loop for listening
+    while (true) {
+        cout << "Waiting on port : " << PORT << endl;
+        recvlen = recvfrom(fd, &buffer[0], sizeof(Packet), 0, (struct sockaddr*) &address, &addrlen);
+        cout << "received " << recvlen << " bytes with packet-detail :" << endl;
+        if (recvlen > 0 ) {
+            // buffer[recvlen] = NULL;
+            buffer[0].Print();
+        }
+    }
+
+    return 0;
+}
--- sendfile.cpp
+++ sendfile.cpp
@@ -1,16 +1,54 @@
-#include "Ack.h"
-#include "Packet.h"
+#include "Constants.hpp"
+
+#include "Ack.hpp"
+#include "Packet.hpp"
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    Ack ack(0, 0);
-    Packet packet(0, 0, 0, 0);
+    int fd = 0;
+    char *server = "127.0.0.1";
+    struct sockaddr_in server_address, address;
 
-    cout << ack.validate() << endl;
-    cout << packet.validate() << endl;
+    Packet *buffer = (Packet*) malloc(BUFFER_SIZE * sizeof(Packet));
     
+    //Testing and filling 5 first packets
+    for (int i=0; i<5; i++) {
+        buffer[i] = Packet(i, 0,0);
+    }
+
+    // buffer[0].Print();
+
+    //Create UDP socket
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) <= 0) {
+        cout << "Socket creation error" << endl;
+        return -1;
+    }
+
+    //Bind socket to any IP and a port
+    memset((char *)&server_address, '0', sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY; //INADDR_ANY = 0.0.0.0 = any available IP address
+    server_address.sin_port = htons(PORT);
+
+    //Convert host address to binary format, and store it in server_address 
+    if ((inet_aton(server, &server_address.sin_addr)) < 0) {
+        cout << "inet_aton error" << endl;
+        return -1;
+    }
+
+    for (int i=0; i<5; i++) {
+        cout << "Sending packet " << i << endl;
+        if (sendto(fd, &buffer[i], sizeof(Packet), 0, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+            cout << "Sendto failed" << endl;
+            return -1;
+        }
+    }
+
     return 0;
 }
